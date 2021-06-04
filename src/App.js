44928a701ce9ba597d59370793b7f83cad1680a3
Junior Rodrigues <junior.frontend@gmail.com>
2021-06-03 23:20:00 -0300

    Add FormRegister

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import React, { Component } from 'react';
 import './App.css';
+import FormularioCadastro from './components/FormularioCadastro';
 
 class App extends Component {
     render () {
         return (
             <>
                 <h1>Formulário de Cadastro</h1>
+                <FormularioCadastro />
             </>
         );
     }
