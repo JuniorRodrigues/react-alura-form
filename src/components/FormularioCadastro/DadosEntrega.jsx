7e4f9d53e8f32fbe384dc674a8b1db0f77bff116
Junior Rodrigues <junior.frontend@gmail.com>
2021-06-05 11:45:57 -0300

    Previous form has been isolated and two new steps were created

---
+++ src/components/FormularioCadastro/DadosEntrega.jsx
@@ -0,0 +1,50 @@
+import { Button, TextField } from '@material-ui/core';
+import React from 'react';
+
+function DadosEntrega () {
+    return (
+        <form>
+            <TextField
+                id="cep"
+                label="CEP"
+                type="number"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+            />
+
+            <TextField
+                id="endereco"
+                label="Endereço"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+            />
+
+            <TextField
+                id="numero"
+                label="Número"
+                type="number"
+                variant="outlined"
+                margin="normal"
+            />
+
+            <TextField
+                id="estado"
+                label="Estado"
+                variant="outlined"
+                margin="normal"
+            />
+
+            <TextField
+                id="cidade"
+                label="Cidade"
+                variant="outlined"
+                margin="normal"
+            />
+            <Button variant="contained" color="primary" fullWidth>Finalizar Cadastro</Button>
+        </form>
+    );
+}
+
+export default DadosEntrega;
--- src/components/FormularioCadastro/index.jsx
+++ src/components/FormularioCadastro/index.jsx
@@ -1,85 +1,17 @@
-import React, { useState } from "react";
-import { Button, TextField, FormControlLabel, Switch } from "@material-ui/core";
-import './styles.css';
+import React from "react";
+import DadosEntrega from "./DadosEntrega";
+import DadosPessoais from "./DadosPessoais";
+import DadosUsuario from "./DadosUsuario";
 
 function FormularioCadastro ({ aoEnviar, validarCPF }) {
-    const [ nome, setNome ] = useState('');
-    const [ sobrenome, setSobrenome ] = useState('');
-    const [ cpf, setCpf ] = useState('');
-    const [ promocoes, setPromocoes ] = useState(true);
-    const [ novidades, setNovidades ] = useState(true);
 
-    const [ erros, setErros ] = useState({ cpf: { valido: true, texto: ''}});
 
     return(
-        <form onSubmit={(event) => {
-            event.preventDefault();
-            aoEnviar({nome, sobrenome, cpf, promocoes, novidades});
-        }}>
-            <TextField id="nome"
-                label="Nome"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                value={ nome }
-
-                onChange={(event) => setNome(event.target.value)}
-            />
-
-            <TextField id="sobrenome"
-                label="Sobrenome"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                value={ sobrenome }
-
-                onChange={(event) => setSobrenome(event.target.value)}
-            />
-
-            <TextField id="cpf"
-                label="CPF"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                error={ !erros.cpf.valido }
-                helperText={ erros.cpf.texto }
-                value={ cpf }
-
-                onChange={(event) => setCpf(event.target.value)}
-                onBlur={() => {
-                    const ehValido = validarCPF(cpf);
-                    setErros({ cpf: ehValido });
-                }}
-            />
-
-            <FormControlLabel
-                label="Promoções"
-                control={
-                    <Switch
-                        color="primary"
-                        name="Promoções"
-                        checked={ promocoes }
-
-                        onChange={(event) => setPromocoes(event.target.checked)}
-                    />
-                }
-            />
-
-            <FormControlLabel
-                label="Novidades"
-                control={
-                    <Switch
-                        color="primary"
-                        name="Novidades"
-                        checked={ novidades }
-
-                        onChange={(event) => setNovidades(event.target.checked)}
-                    />
-                }
-            />
-
-            <Button variant="contained" color="primary" type="submit">Cadastrar</Button>
-        </form>
+        <>
+            <DadosPessoais aoEnviar={ aoEnviar } validarCPF={ validarCPF } />
+            <DadosUsuario />
+            <DadosEntrega />
+        </>
     );
 }
 
