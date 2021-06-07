55ca3b81261a948906ef6ab610a0eff6dc25a2db
Junior Rodrigues <junior.frontend@gmail.com>
2021-06-06 21:05:52 -0300

    Add Stepper from material UI

--- src/components/FormularioCadastro/index.jsx
+++ src/components/FormularioCadastro/index.jsx
@@ -1,23 +1,45 @@
-import { Typography } from "@material-ui/core";
-import React, { useState } from "react";
+import { Step, StepLabel, Stepper, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 
 function FormularioCadastro ({ aoEnviar, validarCPF }) {
     const [ etapaAtual, setEtapaAtual ] = useState(0);
+    const [ dadosColetados, setDadosColetados] = useState({});
+
+    useEffect(() => {
+        if (etapaAtual === formularios.length - 1) aoEnviar(dadosColetados)
+    });
 
     const formularios = [
-        <DadosUsuario aoEnviar={ proximo } />,
-        <DadosPessoais aoEnviar={ proximo } validarCPF={ validarCPF } />,
-        <DadosEntrega aoEnviar={ aoEnviar } />
-    ]
+        <DadosUsuario aoEnviar={ coletarDados } />,
+        <DadosPessoais aoEnviar={ coletarDados } validarCPF={ validarCPF } />,
+        <DadosEntrega aoEnviar={ coletarDados } />,
+        <Typography variant="h5">Obrigado pelo Cadastro!</Typography>
+    ];
 
     function proximo () {
         setEtapaAtual(etapaAtual + 1);
     }
 
-    return(formularios[etapaAtual] || <Typography>Erro ao selecionar formulário</Typography>);
+    function coletarDados (dados) {
+        setDadosColetados({ ...dadosColetados, ...dados })
+        proximo();
+    }
+
+    return(
+        <>
+            <Stepper activeStep={ etapaAtual }>
+                <Step><StepLabel>Login</StepLabel></Step>
+                <Step><StepLabel>Pessoal</StepLabel></Step>
+                <Step><StepLabel>Entrega</StepLabel></Step>
+                <Step><StepLabel>Finalização</StepLabel></Step>
+            </Stepper>
+
+            {formularios[etapaAtual] || <Typography>Erro ao selecionar formulário</Typography>}
+        </>
+    );
 }
 
 export default FormularioCadastro;
